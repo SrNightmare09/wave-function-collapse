462c37328b28e5316536b019df47442491227467
SrNightmare09 <srnightmare09@gmail.com>
2023-06-27 16:55:53 +0530

    style: add function declaration

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
-std::vector<std::vector<char>> GRID;
+std::vector<std::vector<char>> GRID = {
+    {'-', '-', '-'},
+    {'-', '-', '-'},
+    {'-', '-', '-'}
+};
+
+std::vector<char> left_collapse(char);
+std::vector<char> up_collapse(char);
+std::vector<char> find_intersection(std::vector<char>, std::vector<char>);
 
 int main() {
 
@@ -12,11 +21,11 @@ int main() {
 
 std::vector<char> left_collapse(char ch) {
     std::unordered_map<char, std::vector<char>> valid_options = {
-        {'b', {'r', 'b'}},
-        {'u', {'u', 'd', 'l'}},
-        {'r', {'u', 'd', 'l'}},
-        {'l', {'r', 'b'}},
-        {'d', {'u', 'd', 'l'}}
+        {'b', {'b', 'r'}},
+        {'u', {'d', 'l', 'u'}},
+        {'r', {'d', 'l', 'u'}},
+        {'l', {'b', 'r'}},
+        {'d', {'d', 'l', 'u'}}
     };
 
     auto it = valid_options.find(ch);
@@ -36,4 +45,10 @@ std::vector<char> up_collapse(char ch) {
     auto it = valid_options.find(ch);
     std::vector<char> valid = it-> second;
     return valid;
+}
+
+std::vector<char> find_intersection(std::vector<char> a, std::vector<char> b) {
+    std::vector<char> intersection;
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(intersection));
+    return intersection;
 }
