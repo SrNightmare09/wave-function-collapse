e3a903c91410e69692d7b8c8a2ee3cc89194b626
SrNightmare09 <srnightmare09@gmail.com>
2023-06-27 17:14:30 +0530

    feat: function to generate the board along the restrictions of the function

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@ std::vector<char> find_intersection(std::vector<char>, std::vector<char>);
 int get_random_num(int, int);
 void update_grid(int, int, char);
 void print_grid(); // testing purposes
-std::vector<char> find_options(char);
 void start();
 
 int main() {
@@ -67,8 +66,9 @@ int get_random_num(int lower, int upper) { // inclusive of both limits
     return random_num;
 }
 
-void update_grid(int row, int column, char id) {
-    GRID[row][column] = id;
+void update_grid(int row, int column, std::vector<char> options) {
+    int random_index = get_random_num(0, options.size());
+    GRID[row][column] = options[random_index];
     return;
 }
 
@@ -89,4 +89,26 @@ void start() {
     int random_index = get_random_num(0, 4);
     update_grid(0, 0, ids[random_index]);
     return;
+}
+
+void generate_grid() {
+    std::vector<char> options;
+    for (int i = 0; i < GRID.size(); i++){
+        for(int j = 0; j < GRID[i].size(); j++) {
+            if (i == 0 && j == 0) {
+                start(); // if first iteration, initialize
+                continue;
+            }
+
+            if (i == 0) { // if its the first row, meaning no up neighbors
+                options = left_collapse(GRID[i][j]);
+                update_grid(i, j, options);
+            }
+
+            else {
+                options = find_intersection(left_collapse(GRID[i][j]), up_collapse(GRID[i][j]));
+                update_grid(i, j, options);
+            }
+        }
+    }
 }
