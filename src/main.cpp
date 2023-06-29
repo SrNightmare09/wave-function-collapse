70ab57f1aff0f7b91915ddac3db049e72e8f515d
SrNightmare09 <srnightmare09@gmail.com>
2023-06-29 15:46:23 +0530

    feat: implement wave function collapse in cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,121 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
 #include <algorithm>
 #include <random>
 
 std::vector<std::vector<char>> GRID = {
-    {'-', '-', '-'},
-    {'-', '-', '-'},
-    {'-', '-', '-'}
+    {' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' '}
 };
 
-std::vector<char> left_collapse(char);
-std::vector<char> up_collapse(char);
-std::vector<char> find_intersection(std::vector<char>, std::vector<char>);
-int get_random_num(int, int);
-void update_grid(int, int, std::vector<char>);
-void print_grid(); // testing purposes
-void start();
-void generate_grid();
-
-int main() {
-
-    generate_grid();
-
-    return 0;
-
-}
-
-std::vector<char> left_collapse(char ch) {
-    std::unordered_map<char, std::vector<char>> valid_options = {
-        {'b', {'b', 'r'}},
-        {'u', {'d', 'l', 'u'}},
-        {'r', {'d', 'l', 'u'}},
-        {'l', {'b', 'r'}},
-        {'d', {'d', 'l', 'u'}}
-    };
-
-    auto it = valid_options.find(ch);
-    std::vector<char> valid = it->second;
-    return valid;
-}
-
-std::vector<char> up_collapse(char ch) {
-    std::unordered_map<char, std::vector<char>> valid_options = {
-        {'b', {'b', 'd'}},
-        {'u', {'b', 'd'}},
-        {'r', {'u', 'l', 'r'}},
-        {'l', {'u', 'l', 'r'}},
-        {'d', {'u', 'l', 'r'}}
-    };
-
-    auto it = valid_options.find(ch);
-    std::vector<char> valid = it-> second;
-    return valid;
-}
-
-std::vector<char> find_intersection(std::vector<char> a, std::vector<char> b) {
-    std::vector<char> intersection;
-    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(intersection));
-    return intersection;
-}
-
-int get_random_num(int lower, int upper) { // inclusive of both limits
+int get_random_num(int lower, int upper) {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> distr(lower, upper);
-    int random_num = distr(gen);
-    return random_num;
+    int random_number = distr(gen);
+    return random_number;
 }
 
-void update_grid(int row, int column, std::vector<char> options) {
-    int random_index = get_random_num(0, options.size());
-    GRID[row][column] = options[random_index];
-    return;
+void start() {
+    std::vector<char> options = {'d', 'u', 'l', 'r', 'b'};
+
+    int rand_index = get_random_num(0, options.size() - 1);
+    GRID[0][0] = options[rand_index];
 }
 
-void print_grid() {
+std::vector<char> roof_collapse(char ch) {
+    std::unordered_map<char, std::vector<char>> possible_connections = {
+        {'d', {'l', 'r', 'u'}},
+        {'u', {'b', 'd'}},
+        {'l', {'l', 'r', 'u'}},
+        {'r', {'l', 'r', 'u'}},
+        {'b', {'b', 'd'}}
+    };
 
+    std::vector<char> options;
+
+    auto it = possible_connections.find(ch);
+
+    options = it->second;
+
+    return options;
+}
+
+std::vector<char> left_collapse(char ch) {
+    std::unordered_map<char, std::vector<char>> possible_connections = {
+        {'d', {'l', 'd', 'u'}},
+        {'u', {'l', 'd', 'u'}},
+        {'l', {'b', 'r'}},
+        {'r', {'l', 'd', 'u'}},
+        {'b', {'b', 'r'}}
+    };
+
+    std::vector<char> options;
+
+    auto it = possible_connections.find(ch);
+
+    options = it->second;
+
+    return options;
+}
+
+void update_grid(int row, int col, std::vector<char> options) {
+    int rand_num = get_random_num(0, options.size() - 1);
+    GRID[row][col] = options[rand_num];
+}
+
+void collapse() {
+    for (int i = 0; i < GRID.size(); i++) {
+        for (int j = 0; j < GRID[i].size(); j++) {
+
+            if (i == 0 && j == 0) {
+                start();
+                continue;
+            }
+
+            if (i == 0) {
+                std::vector<char> valid_options = left_collapse(GRID[i][j-1]);
+                update_grid(i, j, valid_options);
+                continue;
+            }
+
+            if (j == 0) {
+                std::vector<char> valid_options = roof_collapse(GRID[i - 1][j]);
+                update_grid(i, j, valid_options);
+                continue;
+            }
+
+            if (i != 0 && j != 0) {
+                std::vector<char> valid_options_up = roof_collapse(GRID[i - 1][j]);
+                std::vector<char> valid_options_left = left_collapse(GRID[i][j - 1]);
+                std::sort(valid_options_up.begin(), valid_options_up.end());
+                std::sort(valid_options_left.begin(), valid_options_left.end());
+                std::vector<char> intersection;
+
+                std::set_intersection(valid_options_left.begin(), valid_options_left.end(), valid_options_up.begin(), valid_options_up.end(), std::back_inserter(intersection));
+
+                update_grid(i, j, intersection);
+                continue;
+            }
+
+        }
+    }
+}
+
+void printboard() {
     for (int i = 0; i < GRID.size(); i++) {
         for (int j = 0; j < GRID[i].size(); j++) {
             std::cout << GRID[i][j] << " ";
         }
+
         std::cout << std::endl;
     }
-
 }
 
-void start() {
-    // generate random index for a tile to put it at the upper corner of the grid
-    std::vector<char> ids = {'b', 'u', 'l', 'r', 'd'};
-    update_grid(0, 0, ids);
-    return;
-}
-
-void generate_grid() {
-    std::vector<char> options;
-    for (int i = 0; i < GRID.size(); i++){
-        for(int j = 0; j < GRID[i].size(); j++) {
-            if (i == 0 && j == 0) {
-                start(); // if first iteration, initialize
-                continue;
-            }
-
-            if (i == 0) { // if its the first row, meaning no up neighbors
-                options = left_collapse(GRID[i][j]);
-                update_grid(i, j, options);
-            }
-
-            else {
-                options = find_intersection(left_collapse(GRID[i][j]), up_collapse(GRID[i][j]));
-                update_grid(i, j, options);
-            }
-        }
-    }
+int main () {
+    collapse();
+    printboard();
 }
